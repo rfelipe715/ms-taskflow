78014ff094e5bebb4160e55e760391087cfaf2dc
Felipe <115050091+WolfMozart8@users.noreply.github.com>
2026-04-18 00:34:19 -0400

    first commit

---
+++ src/main/java/cl/duoc/ms_taskflow/MsTaskflowApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_taskflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTaskflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTaskflowApplication.class, args);
+	}
+
+}
