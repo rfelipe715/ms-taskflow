183dac535a60fab60aa489656e0e9f5bf2beedcf
Felipe <115050091+WolfMozart8@users.noreply.github.com>
2026-04-18 00:55:02 -0400

    add TaskRepository

---
+++ src/main/java/cl/duoc/ms_taskflow/repository/TaskRepository.java
@@ -0,0 +1,30 @@
+package cl.duoc.ms_taskflow.repository;
+
+import cl.duoc.ms_taskflow.model.Task;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class TaskRepository {
+
+    private Map<Long, Task> data = new HashMap<>();
+
+    public List<Task> findAll() {
+        return new ArrayList<>(data.values());
+    }
+
+    public Optional<Task> findById(Long id) {
+        return Optional.ofNullable(data.get(id));
+    }
+
+    public Task save(Task task) {
+        data.put(task.getId(), task);
+        return task;
+    }
+
+    public void delete(Long id) {
+        data.remove(id);
+    }
+
+}
